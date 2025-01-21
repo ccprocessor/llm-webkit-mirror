90d0a484c72371a6e8d15982a725cfb996dfc096
drunkpig <xuchao@pjlab.org.cn>
2025-01-21 15:09:41 +0800

    feat: content list to markdown

--- tests/llm_web_kit/pipeline/test_pipeline_suit.py
+++ tests/llm_web_kit/pipeline/test_pipeline_suit.py
@@ -140,5 +140,5 @@ class TestPipelineSuitHTML(unittest.TestCase):
         # txt格式
         txt_content = result.get_content_list().to_txt()
         self.assertEqual(len(txt_content), 727)
-        self.assertNotEquals(txt_content[-2], '\n')
+        self.assertNotEqual(txt_content[-2], '\n')
         self.assertEqual(txt_content[-1], '\n')
