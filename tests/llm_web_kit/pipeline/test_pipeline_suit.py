72d4fad10c3d67702dbbace47d6466b3ff5a9f66
wuziming <wuziming@sensetime.com>
2025-02-06 14:59:57 +0800

    feat: add code pre test case

--- tests/llm_web_kit/pipeline/test_pipeline_suit.py
+++ tests/llm_web_kit/pipeline/test_pipeline_suit.py
@@ -66,7 +66,7 @@ class TestPipelineSuitHTML(unittest.TestCase):
             for line in f:
                 self.data_json.append(json.loads(line.strip()))
 
-        assert len(self.data_json) == 5
+        assert len(self.data_json) == 6
 
     def test_html_pipeline(self):
         """Test HTML pipeline with sample data."""
@@ -237,3 +237,21 @@ class TestPipelineSuitHTML(unittest.TestCase):
         result = pipeline.extract(input_data)
         md_content = result.get_content_list().to_nlp_md()
         self.assertIn('The descendant of `StandardizerActionRunner` interface has to provide', md_content)
+
+    def test_code_pre_mixed(self):
+        pipeline = PipelineSuit(self.pipeline_config)
+        self.assertIsNotNone(pipeline)
+        test_data = self.data_json[5]
+        # Create DataJson from test data
+        input_data = DataJson(test_data)
+        result = pipeline.extract(input_data)
+        self.assertIn("""```
+        this (DEFAULT_SERVER_NAME, DEFAULT_SERVER_PORT);
+```
+
+Test Test Test
+
+```
+ABC
+DEF
+```""", result.get_content_list().to_mm_md())
