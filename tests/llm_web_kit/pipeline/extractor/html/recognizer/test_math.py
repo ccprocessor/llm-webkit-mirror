2f41a50b6894b04e687228d4cadb8ca2b0d9ba1d
彭中星 <PJLAB\pengzhongxing@pjnl104220117l.pjlab.org>
2025-02-13 19:18:08 +0800

    init2

--- tests/llm_web_kit/pipeline/extractor/html/recognizer/test_math.py
+++ tests/llm_web_kit/pipeline/extractor/html/recognizer/test_math.py
@@ -85,13 +85,13 @@ TEST_CASES_HTML = [
     #     'base_url': 'https://en.m.wikipedia.org/wiki/Variance',
     #     'expected': 'assets/ccmath/wikipedia_1_math_annotation_1.html'
     # },
-    {
-        'input': [
-            'assets/ccmath/mathjax-mml-chtml.html',
-        ],
-        'base_url': 'https://mathjax.github.io/MathJax-demos-web/tex-chtml.html',
-        'expected': 'assets/ccmath/mathjax-mml-chtml_1.html'
-    },
+    # {
+    #     'input': [
+    #         'assets/ccmath/mathjax-mml-chtml.html',
+    #     ],
+    #     'base_url': 'https://mathjax.github.io/MathJax-demos-web/tex-chtml.html',
+    #     'expected': 'assets/ccmath/mathjax-mml-chtml_1.html'
+    # },
     # # img latex.php
     # {
     #     'input': ['assets/ccmath/geoenergymath_img.html'],
@@ -125,6 +125,13 @@ TEST_CASES_HTML = [
     #     'base_url': 'https://www.intmath.com/cg5/katex-mathjax-comparison.php',
     #     'expected': 'assets/ccmath/mathtex_script_type_1.html'
     # },
+    {
+        'input': [
+            'assets/ccmath/mathjax-mml-chtml_prefix.html',
+        ],
+        'base_url': 'https://mathjax.github.io/MathJax-demos-web/tex-chtml.html',
+        'expected': 'assets/ccmath/mathjax-mml-chtml_prefix_1.html'
+    },
 ]
 
 TEST_EQUATION_TYPE = [
