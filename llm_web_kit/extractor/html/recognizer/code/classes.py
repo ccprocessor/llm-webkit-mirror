570c9d0fa506b9db3ba47070360e52ad5c314229
wuziming <wuziming@sensetime.com>
2025-03-31 17:20:12 +0800

    feat: shorten

--- llm_web_kit/extractor/html/recognizer/code/classes.py
+++ llm_web_kit/extractor/html/recognizer/code/classes.py
@@ -6,42 +6,26 @@ from llm_web_kit.extractor.html.recognizer.recognizer import CCTag
 
 
 def modify_tree(root: HtmlElement) -> None:
-    for maybe_code_root in root.iter(None):
+    for maybe_code_root in root.xpath(".//*[@class]"):
         assert isinstance(maybe_code_root, HtmlElement)
-
-        hit = False
-        classes = maybe_code_root.classes
-        for class_name in classes:
-            if 'code' in class_name:
-                hit = True
-        if not hit:
+        if not any(['code' in class_name for class_name in maybe_code_root.classes]):
             continue
 
-        hit = False
-        for _ in maybe_code_root.iter(CCTag.CC_CODE):
-            hit = True
-            break
-        if not hit:
-            replace_node_by_cccode(maybe_code_root, 'classname', False, False)
+        if len(maybe_code_root.xpath(f".//{CCTag.CC_CODE}")) > 0:
+            continue
+
+        replace_node_by_cccode(maybe_code_root, 'classname', False, False)
 
 
 def detect(root: HtmlElement) -> bool:
-    for maybe_code_root in root.iter(None):
+    for maybe_code_root in root.xpath(".//*[@class]"):
         assert isinstance(maybe_code_root, HtmlElement)
-
-        hit = False
-        classes = maybe_code_root.classes
-        for class_name in classes:
-            if 'code' in class_name:
-                hit = True
-        if not hit:
+        if not any(['code' in class_name for class_name in maybe_code_root.classes]):
             continue
 
-        hit = False
-        for _ in maybe_code_root.iter(CCTag.CC_CODE):
-            hit = True
-            break
-        if not hit:
-            return True
+        if len(maybe_code_root.xpath(f".//{CCTag.CC_CODE}")) > 0:
+            continue
+
+        return True
 
     return False
