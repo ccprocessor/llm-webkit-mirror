900efaf30b1dccbc8f15ae77016e6e482f110380
chupei <njuchupei@gmail.com>
2025-05-14 17:16:11 +0800

    x

--- jupyter/domain_clustering/statics/domain-hash-id-statics.py
+++ jupyter/domain_clustering/statics/domain-hash-id-statics.py
@@ -1,11 +1,12 @@
 import io
 
 import pyspark.sql.functions as F
-from cc_store.libs.domain import compute_domain_hash
 from pyspark.sql import Window
 from pyspark.sql.types import IntegerType
 from xinghe.s3 import put_s3_object_with_retry
 from xinghe.spark import new_spark_session, read_any_path
+
+from jupyter.domain_clustering.libs.domain import compute_domain_hash
 
 # 配置参数
 hash_count = 10000  # 域名哈希桶数量
