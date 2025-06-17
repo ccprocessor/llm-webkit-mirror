c4674efae40ee3ea22899afc9cb92cfbf3f65730
Una_Una <132733906+ddfinshes@users.noreply.github.com>
2025-06-17 16:03:34 +0800

    识别部分bug修复 (#456)

--- tests/llm_web_kit/extractor/test_extractor_chain.py
+++ tests/llm_web_kit/extractor/test_extractor_chain.py
@@ -63,7 +63,7 @@ class TestExtractorChain(unittest.TestCase):
                     continue
                 self.data_json.append(json.loads(line))
 
-        assert len(self.data_json) == 90
+        assert len(self.data_json) == 97
 
         # Config for HTML extraction
         self.config = load_pipe_tpl('html-test')
@@ -763,3 +763,79 @@ A few explanations on why certain things in business are so.
         # print('Markdown Content:', md_content)
         self.assertIn(r'$\lim\limits_{x \to 1}\dfrac{x^2-1}{x-1}$', md_content)
         self.assertIn(r'\begin{aligned} \frac{f(1.01)-f(1)}{1.01-1} &= \frac{1.01^2-1^2}{0.01} \\ &= \frac{0.0201}{0.01} \\ &= 2.01\end{aligned}', md_content)
+
+    def test_double_ul(self):
+        """测试双重ul标签."""
+        chain = ExtractSimpleFactory.create(self.config)
+        self.assertIsNotNone(chain)
+        test_data = self.data_json[90]
+        input_data = DataJson(test_data)
+        result = chain.extract(input_data)
+        content_md = result.get_content_list().to_nlp_md()
+        assert 'Wheels and Tyres' in content_md
+
+    def test_other_space(self):
+        """测试括号、双引单引号等中文符号导致的空格."""
+        chain = ExtractSimpleFactory.create(self.config)
+        self.assertIsNotNone(chain)
+        test_data = self.data_json[91]
+        input_data = DataJson(test_data)
+        result = chain.extract(input_data)
+        content_md = result.get_content_list().to_nlp_md()
+        assert '(APC)' in content_md
+        test_data = self.data_json[92]
+        input_data = DataJson(test_data)
+        result = chain.extract(input_data)
+        content_md = result.get_content_list().to_nlp_md()
+        assert '“I do swear by the Day of Resurrection.' in content_md
+
+    def test_classname_to_code(self):
+        """测试由于classname导致的audio、list识别为code的情况."""
+        chain = ExtractSimpleFactory.create(self.config)
+        self.assertIsNotNone(chain)
+        test_data = self.data_json[93]
+        input_data = DataJson(test_data)
+        result = chain.extract(input_data)
+        content_list = result.get_content_list().to_dict()[0]
+        types = []
+        # 1
+        for i in range(len(content_list)):
+            types.append(content_list[i]['type'])
+        with open('output.jsonl', 'w') as f:
+            f.write(result.get_content_list().to_json())
+        assert 'code' not in types
+        test_data = self.data_json[94]
+        input_data = DataJson(test_data)
+        result = chain.extract(input_data)
+        content_list = result.get_content_list().to_dict()[0]
+        types = []
+
+        for i in range(len(content_list)):
+            types.append(content_list[i]['type'])
+        assert 'code' not in types
+
+    def test_sup_escape_error(self):
+        """测试<sup>和<sub>被转义成<sup&gt;和<sub&gt;导致上下标失效的情况."""
+        chain = ExtractSimpleFactory.create(self.config)
+        self.assertIsNotNone(chain)
+        test_data = self.data_json[95]
+        input_data = DataJson(test_data)
+        result = chain.extract(input_data)
+        content_md = result.get_content_list().to_nlp_md()
+        # print(content_md)
+        assert '<sup&gt;' not in content_md and '<sub&gt;' not in content_md
+
+    def test_item_notext_error(self):
+        """测试跳过list item中无实际内容的情况."""
+        chain = ExtractSimpleFactory.create(self.config)
+        self.assertIsNotNone(chain)
+        test_data = self.data_json[96]
+        input_data = DataJson(test_data)
+        result = chain.extract(input_data)
+        content_list = result.get_content_list().to_dict()[0]
+        first_item_list = {}
+        for i in range(len(content_list)):
+            if content_list[i]['type'] == 'list':
+                first_item_list = content_list[i]['content']['items']
+                break
+        assert len(first_item_list) == 0
