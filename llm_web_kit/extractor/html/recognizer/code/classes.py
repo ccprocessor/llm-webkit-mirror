181b0d71851f0fcee8005550a82920c4c4fb5a4f
ddfinshes <d15528029615@163.com>
2025-06-17 15:36:16 +0800

    extractor bug

--- llm_web_kit/extractor/html/recognizer/code/classes.py
+++ llm_web_kit/extractor/html/recognizer/code/classes.py
@@ -17,9 +17,6 @@ def modify_tree(root: HtmlElement) -> None:
         # 应对list或者audio被识别为code的情况
         if maybe_code_root.tag in no_code_tags:
             continue
-        if maybe_code_root.tag == 'div':
-            print([child for child in maybe_code_root.iterchildren()])
-
         if maybe_code_root.tag == 'div' and (any([child.tag in no_code_tags for child in maybe_code_root.iterchildren()]) or len([child for child in maybe_code_root.iterchildren()]) == 0):
             continue
         if len(maybe_code_root.xpath(f'.//{CCTag.CC_CODE}')) > 0:
