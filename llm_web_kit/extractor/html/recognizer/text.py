52bd6d8fa261b08d738809e004d525a05d38be6a
linfeng <56671143+LollipopsAndWine@users.noreply.github.com>
2025-08-08 18:27:17 +0800

    refactor: 重构simple (#511)

--- llm_web_kit/extractor/html/recognizer/text.py
+++ llm_web_kit/extractor/html/recognizer/text.py
@@ -195,7 +195,7 @@ class TextParagraphRecognizer(BaseHTMLElementRecognizer):
             return self.replace_entities(txt.strip(), entities_map)
         else:
             # 根据text1的最后一个字符和text2的第一个字符判断两个text之间的连接
-            if (text2[0] in string.punctuation) or (text2[0] in special_symbols) or (text2[0] in other_symbols) or (text1 and text1[-1] in other_symbols):
+            if (text2 and text2[0] in string.punctuation) or (text2 and text2[0] in special_symbols) or (text2 and text2[0] in other_symbols) or (text1 and text1[-1] in other_symbols):
                 words_sep = ''
             else:
                 words_sep = ' '
--- llm_web_kit/simple.py
+++ llm_web_kit/simple.py
@@ -5,10 +5,12 @@ from datetime import datetime
 
 from llm_web_kit.config.cfg_reader import load_pipe_tpl
 from llm_web_kit.extractor.extractor_chain import ExtractSimpleFactory
-from llm_web_kit.extractor.html.extractor import (
-    HTMLPageLayoutType, MagicHTMLFIleFormatorExtractor,
-    NoClipHTMLFIleFormatorExtractor)
 from llm_web_kit.input.datajson import DataJson
+
+
+class PipeType:
+    HTML = 'html'
+    NOCLIP = 'noclip_html'
 
 
 class ExtractorType:
@@ -19,24 +21,32 @@ class ExtractorType:
 
 class ExtractorFactory:
     """factory class for extractor."""
-    html_extractor = None
+    magic_html_extractor = None
+    noclip_html_extractor = None
     pdf_extractor = None
     ebook_extractor = None
 
     @staticmethod
-    def get_extractor(extractor_type: str):
+    def get_extractor(extractor_type: str, pipe_tpl_name: str):
         if extractor_type == ExtractorType.HTML:
-            if ExtractorFactory.html_extractor is None:
-                extractor_cfg = load_pipe_tpl('html')
-                chain = ExtractSimpleFactory.create(extractor_cfg)
-                ExtractorFactory.html_extractor = chain
-            return ExtractorFactory.html_extractor
+            if pipe_tpl_name == PipeType.HTML:
+                if ExtractorFactory.magic_html_extractor is None:
+                    extractor_cfg = load_pipe_tpl(pipe_tpl_name)
+                    chain = ExtractSimpleFactory.create(extractor_cfg)
+                    ExtractorFactory.magic_html_extractor = chain
+                return ExtractorFactory.magic_html_extractor
+            if pipe_tpl_name == PipeType.NOCLIP:
+                if ExtractorFactory.noclip_html_extractor is None:
+                    extractor_cfg = load_pipe_tpl(pipe_tpl_name)
+                    chain = ExtractSimpleFactory.create(extractor_cfg)
+                    ExtractorFactory.noclip_html_extractor = chain
+                return ExtractorFactory.noclip_html_extractor
         else:
             raise ValueError(f'Invalid extractor type: {extractor_type}')
 
 
 def __extract_main_html_by_no_clip_html(url:str, html_content: str, raw_html:str) -> DataJson:
-    extractor = NoClipHTMLFIleFormatorExtractor(load_pipe_tpl('noclip_html'))
+    extractor = ExtractorFactory.get_extractor(ExtractorType.HTML, PipeType.NOCLIP)
     if raw_html == '':
         raw_html = html_content
     input_data_dict = {
@@ -54,14 +64,8 @@ def __extract_main_html_by_no_clip_html(url:str, html_content: str, raw_html:str
     return result
 
 
-def __extract_main_html_by_maigic_html(url:str, html_str: str, page_layout_type:str) -> DataJson:
-    magic_html_extractor = MagicHTMLFIleFormatorExtractor(load_pipe_tpl('html'))
-    main_html, method, title = magic_html_extractor._extract_main_html(html_str, url, page_layout_type)
-    return main_html, title
-
-
 def __extract_html(url:str, html_content: str) -> DataJson:
-    extractor = ExtractorFactory.get_extractor(ExtractorType.HTML)
+    extractor = ExtractorFactory.get_extractor(ExtractorType.HTML, PipeType.HTML)
     input_data_dict = {
         'track_id': str(uuid.uuid4()),
         'url': url,
@@ -94,7 +98,10 @@ def extract_html_to_mm_md(url:str, html_content: str, clip_html=True, raw_html='
     return result.get_content_list().to_mm_md()
 
 
-def extract_main_html_by_maigic_html(url:str, html_str: str, page_layout_type:str = HTMLPageLayoutType.LAYOUT_ARTICLE) -> str:
-    """extract main html."""
-    result = __extract_main_html_by_maigic_html(url, html_str, page_layout_type)
-    return result[0], result[1]
+def extract_main_html(url:str, html_content: str, clip_html=True, raw_html='') -> str:
+    if clip_html:
+        result = __extract_html(url, html_content)
+    else:
+        result = __extract_main_html_by_no_clip_html(url, html_content, raw_html)
+    main_html = result.get('main_html')
+    return main_html
