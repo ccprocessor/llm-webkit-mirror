a857bee8c4fbb1325690ff1f5fe69d086c2b3aba
Yanggq <1041206149@qq.com>
2025-10-23 17:29:25 +0800

    删除replace_math逻辑

--- llm_web_kit/extractor/html/recognizer/ccmath.py
+++ llm_web_kit/extractor/html/recognizer/ccmath.py
@@ -5,8 +5,7 @@ from overrides import override
 
 from llm_web_kit.exception.exception import (
     HtmlMathMathjaxRenderRecognizerException, HtmlMathRecognizerException)
-from llm_web_kit.extractor.html.recognizer.cc_math import (tag_common_modify,
-                                                           tag_img, tag_math,
+from llm_web_kit.extractor.html.recognizer.cc_math import (tag_img, tag_math,
                                                            tag_mjx, tag_script)
 from llm_web_kit.extractor.html.recognizer.cc_math.common import (CCMATH, CSDN,
                                                                   ZHIHU)
@@ -140,14 +139,14 @@ class MathRecognizer(BaseHTMLElementRecognizer):
                     tag_script.process_zhihu_custom_tag(self.cm, math_render_type, node)
 
                 # tag = span， class 为 math-containerm， 或者 mathjax 或者 wp-katex-eq
-                if node.tag == 'span' and node.get('class') and (
-                        'math-container' in node.get('class') or
-                        'mathjax' in node.get('class') or
-                        'wp-katex-eq' in node.get('class') or
-                        'x-ck12-mathEditor' in node.get('class') or
-                        'tex' in node.get('class')
-                ):
-                    tag_common_modify.modify_tree(self.cm, math_render_type, original_html, node, parent)
+                # if node.tag == 'span' and node.get('class') and (
+                #         'math-container' in node.get('class') or
+                #         'mathjax' in node.get('class') or
+                #         'wp-katex-eq' in node.get('class') or
+                #         'x-ck12-mathEditor' in node.get('class') or
+                #         'tex' in node.get('class')
+                # ):
+                #     tag_common_modify.modify_tree(self.cm, math_render_type, original_html, node, parent)
 
                 # math tags
                 if node.tag == 'math' or node.tag.endswith(':math'):
